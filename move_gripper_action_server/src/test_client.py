07fa8b9d9b429bcce304acb38f1427eff58468a2
Marc <stelter2@uni-bremen.de>
2019-12-11 15:09:25 +0100

    Changed root for actionserver; Cancel all other goals for giskard on new message timeout increased to 60 testclient orientation adjusted

--- move_gripper_action_server/src/test_client.py
+++ move_gripper_action_server/src/test_client.py
@@ -3,6 +3,7 @@ import rospy
 import actionlib
 from manipulation_action_msgs.msg import MoveGripperGoal, MoveGripperAction
 from geometry_msgs.msg import PoseStamped, Point, Quaternion
+from giskardpy import tfwrapper
 
 def test_client():
     client = actionlib.SimpleActionClient('move_gripper_server', MoveGripperAction)
@@ -16,13 +17,14 @@ def test_client():
     pose.header.stamp = rospy.Time.now()
     pose.header.frame_id = u'map'
     pose.header.stamp = rospy.get_rostime()
-    pose.pose.position = Point(0.2,0,0.5)
-    pose.pose.orientation = Quaternion(0,0,0,1)
+    pose.pose.position = Point(1, 0.0, 0.05)
+    pose.pose.orientation = Quaternion(0.7,0,-0.7,0)
+
+
 
     goal = MoveGripperGoal(goal_pose= pose)
-    
 
-    # Sends the goal to the action server.
+# Sends the goal to the action server.
     client.send_goal(goal)
 
     # Waits for the server to finish performing the action.
@@ -36,6 +38,7 @@ if __name__ == '__main__':
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
         rospy.init_node('test_client_py')
+#        print(tfwrapper.lookup_transform(u'map',u'hand_palm_link'))
         result = test_client()
         print("Result:", result)
     except rospy.ROSInterruptException:
