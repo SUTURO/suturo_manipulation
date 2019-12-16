c120a01865ed6c6dca4a32d8254f7a1979f6a8d3
Marc <stelter2@uni-bremen.de>
2019-12-16 14:38:05 +0100

    some refactoring of code

--- place_action_server/src/place_server.py
+++ place_action_server/src/place_server.py
@@ -25,34 +25,24 @@ class PlaceServer():
         print("Order recieved")
         self._result.error_code = self._result.FAILED
 
-        ## TODO: Hartcoded object we grasped.
-        ## TODO: Object should have been attached already. Decide on a id for the grasped object in giskard
-        pose = PoseStamped()
-        pose.header.frame_id = u'hand_palm_link'
-        pose.header.stamp = rospy.Time.now()
-        pose.pose.position = Point(0,0,0)
-        pose.pose.orientation = Quaternion(0,0.7,0,0.7)
+        ##TODO change message to send this / Or get from tf-tree
+        object_frame_id = u'grasped_object'
 
         ## Calculate offset for hand palm link because of grasped object
         ## TODO: Find out if giskard can handle this. if not make it more generic and put it in an extra library
-        goal.goal_pose.pose.position = Point(
-            goal.goal_pose.pose.position.x,
-            goal.goal_pose.pose.position.y,
-            goal.goal_pose.pose.position.z + 0.125
-        )
+#        goal.goal_pose.pose.position = Point(
+#            goal.goal_pose.pose.position.x,
+#            goal.goal_pose.pose.position.y,
+#            goal.goal_pose.pose.position.z + 0.125
+#        )
 
-        #TODO: Get object dimension
-        self._giskard_wrapper.add_cylinder(name=u'box', size=(0.25,0.07,0.07), pose=pose)
-        #TODO: Add object ro gripper
-        self._giskard_wrapper.attach_object(name=u'box', link_frame_id=u'hand_palm_link')
         #TODO: Move gripper with object to goal
-        self._giskard_wrapper.set_cart_goal(self._root, u'hand_palm_link', goal.goal_pose)
+        self._giskard_wrapper.set_cart_goal(self._root, object_frame_id, goal.goal_pose)
         self._giskard_wrapper.plan_and_execute(wait=False)
-        result = self._giskard_wrapper.get_result(rospy.Duration(60))
+        result = self._giskard_wrapper.get_result(rospy.Duration(30))
 
-        #TODO: Release gripper
-
-        #TODO: Detach object from gripper
+        # Release gripper
+        # Detach object from gripper
         goal_js ={
             u'hand_l_spring_proximal_joint': 0.7,
             u'hand_r_spring_proximal_joint': 0.7
@@ -60,8 +50,7 @@ class PlaceServer():
         if result.error_code == result.SUCCESS:
             self._giskard_wrapper.set_joint_goal(goal_js)
             result = self._giskard_wrapper.plan_and_execute()
-            self._giskard_wrapper.detach_object(u'box')
-            self._giskard_wrapper.remove_object(u'box')
+            self._giskard_wrapper.detach_object(object_frame_id)
 
 
         ##TODO: load default pose from json file
