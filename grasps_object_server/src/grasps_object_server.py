f6c80b3c09b5524a889477536c73bf7f604ea8e9
Fabian Weihe <weihe@uni-bremen.de>
2019-12-16 17:20:28 +0100

    hardcoded grasp position

--- grasps_object_server/src/grasps_object_server.py
+++ grasps_object_server/src/grasps_object_server.py
@@ -5,7 +5,7 @@ import actionlib
 from manipulation_action_msgs.msg import GraspAction, GraspFeedback, GraspResult
 from giskardpy.python_interface import GiskardWrapper
 from giskardpy import tfwrapper
-#import hsrb_interface
+from geometry_msgs.msg import PoseStamped, Point, Quaternion
 
 
 class GraspsObjectServer:
@@ -21,31 +21,35 @@ class GraspsObjectServer:
         print("GraspsActionServer greats its masters and is waiting for orders")
 
     def execute_cb(self, goal):
-        print("try to do something")
+        grasped_object = u'grasped_object'
 
-        #remove before demo
-        self._giskard_wrapper.set_joint_goal({u'hand_l_spring_proximal_joint: 0.7, u', u'hand_r_spring_proximal_joint: 0.7'})
-        self._giskard_wrapper.plan_and_execute()
+        pose = PoseStamped()
+        pose.header.frame_id = u'hand_palm_link'
+        pose.header.stamp = rospy.Time.now()
+        pose.pose.position = Point(0, 0.035, 0)
+        pose.pose.orientation = Quaternion(0, 0.7, 0, 0.7)
 
         self._result.error_code = self._result.FAILED
 
-        self._giskard_wrapper.add_cylinder(u'cylinder', (0.25, 0.07), u'map', (1.1, 0, 0.8), (0, 0, 0, 1))
+        self._giskard_wrapper.add_cylinder(name=grasped_object, size=(0.25, 0.07), pose=pose)
 
         # Close the Gripper
-        self._giskard_wrapper.set_joint_goal({u'hand_l_spring_proximal_joint: 0.4, u', u'hand_r_spring_proximal_joint: 0.4'})
+        self._giskard_wrapper.set_joint_goal({u'hand_l_spring_proximal_joint': 0.2,
+                                              u'hand_r_spring_proximal_joint': 0.2})
         self._giskard_wrapper.plan_and_execute()
 
         # Attach object
-        self._giskard_wrapper.attach_cylinder(u'cylinder')
+        self._giskard_wrapper.attach_object(name=grasped_object, link_frame_id=u'hand_palm_link')
 
         # Pose to move with an attatched Object
-        self._giskard_wrapper.set_joint_goal({u'arm_roll_joint': 1.57, u'wrist_flex_joint': -1.37})
+        self._giskard_wrapper.set_joint_goal({u'arm_roll_joint': 1.57,
+                                              u'wrist_flex_joint': -1.37})
         self._giskard_wrapper.plan_and_execute()
 
         result = self._giskard_wrapper.get_result()
 
-        self._feedback.tf_gripper_to_object = tfwrapper.lookup_transform(goal.object_frame_id, u'hand_palm_link')
-        self._feedback.gripper_joint_state = u'hand_l_spring_proximal_joint' + u'hand_r_spring_proximal_joint'
+        # self._feedback.tf_gripper_to_object = tfwrapper.lookup_transform(goal.object_frame_id, u'hand_palm_link')
+        # self._feedback.gripper_joint_state = u'hand_l_spring_proximal_joint' + u'hand_r_spring_proximal_joint'
 
         if result and result.error_code == result.SUCCESS:
             self._result.error_code = self._result.SUCCESS
@@ -71,10 +75,24 @@ class GraspsObjectServer:
         GiskardWrapper.add_box(giskard_wrapper, 'box', (1, 1, 0.5), u'map', (1.5, 0, 0.5), (0, 0, 0, 1))
         GiskardWrapper.add_box(giskard_wrapper, 'grasps_obj', (0.1, 0.1, 0.1), u'map', (1.1, 0, 0.8), (0, 0, 0, 1))
 
+    def attach_test(self):
+        giskard_wrapper = GiskardWrapper()
+
+        pose = PoseStamped()
+        pose.header.frame_id = u'hand_palm_link'
+        pose.header.stamp = rospy.Time.now()
+        pose.pose.position = Point(0, 0, 0)
+        pose.pose.orientation = Quaternion(0, 0.7, 0, 0.7)
+
+        giskard_wrapper.add_cylinder(name=u'box', size=(0.25, 0.07), pose=pose)
+        # TODO: Add object ro gripper
+        giskard_wrapper.attach_object(name=u'box', link_frame_id=u'hand_palm_link')
+
 
 if __name__ == '__main__':
     rospy.init_node('grasps_object_server')
     server = GraspsObjectServer(rospy.get_name())
     #server.place_object()
     #server.hardcode_grasp()
+    #server.attach_test()
     rospy.spin()
