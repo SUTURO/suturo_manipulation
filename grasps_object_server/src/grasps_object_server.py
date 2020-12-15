cb03039094a272e7520f4de3c3fa676b502a66ed
marc <stelter2@uni-bremen.de>
2020-12-15 19:10:36 +0100

    merge with clement plus some refactoring

--- grasps_object_server/src/grasps_object_server.py
+++ grasps_object_server/src/grasps_object_server.py
@@ -9,13 +9,8 @@ import rospy
 import actionlib
 from manipulation_action_msgs.msg import GraspAction, GraspFeedback, GraspResult, ObjectInGripper
 from giskardpy.python_interface import GiskardWrapper
-from geometry_msgs.msg import PoseStamped, Point, Quaternion
-from tf.transformations import quaternion_from_euler, quaternion_multiply
-from listener import Listener
-from sensor_msgs.msg import JointState
-from force_checking import ForceSensorCapture
-
-from giskardpy import tfwrapper, utils
+from geometry_msgs.msg import PoseStamped, Point
+from giskardpy import tfwrapper
 from tmc_control_msgs.msg import GripperApplyEffortAction, GripperApplyEffortGoal
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
 from trajectory_msgs.msg import JointTrajectoryPoint
@@ -40,7 +35,7 @@ class GraspsObjectServer:
         self._gripper_controller = actionlib.SimpleActionClient(u'/hsrb/gripper_controller/follow_joint_trajectory',
                                                                 FollowJointTrajectoryAction)
         self._gripper_controller.wait_for_server()
-        self._obj_in_gripper_pub = rospy.Publisher("object_in_gripper", ObjectInGripper)
+        self._obj_in_gripper_pub = rospy.Publisher("object_in_gripper", ObjectInGripper, queue_size=10)
         self._as.start()
         print("GraspsActionServer greets its masters and is waiting for orders")
 
@@ -48,49 +43,46 @@ class GraspsObjectServer:
 
         print("Recieve Order. grasp", goal)
         self._giskard_wrapper.interrupt()
-        in_gripper = False
-        # grasped_object = u'grasped_object'
-        pose = PoseStamped()
-        pose.header = goal.goal_pose.header
-        pose.pose.position = goal.goal_pose.pose.position
 
         # Set initial result value.
         self._result.error_code = self._result.FAILED
+        goal_pose = goal.goal_pose
 
+        #open gripper
         self.set_gripper_joint_position(1.2)
-        
+        in_gripper = False
+
         hsr_transform = tfwrapper.lookup_transform('map', 'base_footprint')
-        # grasp_mode
-        root_tip_orientation = None
+        base_tip_rotation = None
         step = None
         if goal.grasp_mode == goal.FRONT:
-            pose.pose.position.z += goal.object_size.z / 2.0
-            root_tip_orientation = self.FRONT_ROTATION_QUATERNION
+            goal_pose.pose.position.z += goal.object_size.z / 2.0
+            base_tip_rotation = self.FRONT_ROTATION_QUATERNION
             step = 0.1
 
         elif goal.grasp_mode == goal.TOP:
-            pose.pose.position.z += goal.object_size.z
-            rospy.loginfo("ACTUAL TOP GRASPING HEIGHT: " + str(pose))
-            root_tip_orientation = self.TOP_ROTATION_QUATERNION
+            goal_pose.pose.position.z += goal.object_size.z
+            rospy.loginfo("ACTUAL TOP GRASPING HEIGHT: " + str(goal_pose))
+            base_tip_rotation = self.TOP_ROTATION_QUATERNION
 
-        pose.header.stamp = rospy.Time.now()
         # Move the robot in goal position.        
-        # self._giskard_wrapper.avoid_all_collisions(distance=0.3)
-        self._giskard_wrapper.allow_all_collisions()
+        self._giskard_wrapper.avoid_all_collisions(distance=0.3)
         self._giskard_wrapper.allow_collision(body_b=goal.object_frame_id)
-
-
-        self._giskard_wrapper.set_cart_goal_wstep(self._root, u'hand_palm_link', pose, root_tip_orientation, step=step, hsr_transform=hsr_transform)
-		self._giskard_wrapper.plan_and_execute(wait=True)
-
+        self._giskard_wrapper.set_cart_goal_wstep(self._root,
+                                                  u'hand_palm_link',
+                                                  goal_pose,
+                                                  base_tip_rotation=base_tip_rotation,
+                                                  step=step,
+                                                  base_transform=hsr_transform)
         result = self._giskard_wrapper.get_result(rospy.Duration(60))
-        if result and result.SUCCESS in result.error_codes:
 
+        if result and result.SUCCESS in result.error_codes:
             # Close the Gripper
             self.close_gripper_force(0.8)
+            in_gripper = self.object_in_gripper()
 
-            if self.object_in_gripper():
-                # Attach object TODO: Add again once we disable collision for object to grasp | enable after grasp
+            if in_gripper:
+                # Attach object
                 self._giskard_wrapper.attach_object(goal.object_frame_id, u'hand_palm_link')
 
                 obj_in_gri = ObjectInGripper()
@@ -99,24 +91,17 @@ class GraspsObjectServer:
                 obj_in_gri.mode = ObjectInGripper.GRASPED
                 self._obj_in_gripper_pub.publish(obj_in_gri)
 
-                in_gripper = True
-
             # Pose to move with an attached Object
+            start_pose = PoseStamped()
+            start_pose.header.frame_id = "map"
+            start_pose.header.stamp = rospy.Time.now()
+            start_pose.pose.position = Point(hsr_transform.transform.translation.x, hsr_transform.transform.translation.y, hsr_transform.transform.translation.z)
+            start_pose.pose.orientation = hsr_transform.transform.rotation
+            self._giskard_wrapper.set_cart_goal_wstep(self._root, u'base_footprint', start_pose)
 
-            p_temp = PoseStamped()
-            p_temp.header.frame_id = "map"
-            p_temp.header.stamp = rospy.Time.now()
-            p_temp.pose.position = Point(hsr_transform.transform.translation.x, hsr_transform.transform.translation.y, hsr_transform.transform.translation.z)
-            p_temp.pose.orientation = hsr_transform.transform.rotation
-            self._giskard_wrapper.set_cart_goal_wstep(self._root, u'base_footprint', p_temp)
-
-            self._giskard_wrapper.plan_and_execute(wait=True)
-
-            # self._giskard_wrapper.avoid_all_collisions(distance=0.02)
-            self._giskard_wrapper.allow_all_collisions()
+            self._giskard_wrapper.avoid_all_collisions(distance=0.03)
             self._giskard_wrapper.set_joint_goal(rospy.get_param(u'/robot_poses/transport'))
             self._giskard_wrapper.plan_and_execute(wait=True)
-
             result_giskard = self._giskard_wrapper.get_result()
 
             if in_gripper and result_giskard and result_giskard.SUCCESS in result_giskard.error_codes:
@@ -131,7 +116,7 @@ class GraspsObjectServer:
         """
         joint_states = self._giskard_wrapper.get_joint_states(u'/hsrb/joint_states')
         if joint_states.has_key(u'hand_motor_joint'):
-            result = joint_states[u'hand_motor_joint'] <= -0.8
+            result = joint_states[u'hand_motor_joint'] > -0.8
             rospy.loginfo("object_in_gripper: {}".format(result))
             return result
         else:
