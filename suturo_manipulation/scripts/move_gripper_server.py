6d5e019bd80b94532059e019faca277627157f5e
marc <stelter2@uni-bremen.de>
2020-12-17 05:08:45 +0100

    A lot of refactoring. No more redundant code only a little bit of acceptable hardcoded stuff

--- suturo_manipulation/scripts/move_gripper_server.py
+++ suturo_manipulation/scripts/move_gripper_server.py
@@ -4,42 +4,29 @@ import rospy
 import actionlib
 from manipulation_action_msgs.msg import MoveGripperAction, MoveGripperFeedback, MoveGripperResult
 from giskardpy.python_interface import GiskardWrapper
-from giskardpy import tfwrapper
 
-class MoveGripperServer():
+
+class MoveGripperServer:
     _feedback = MoveGripperFeedback()
     _result = MoveGripperResult()
     _root = u'odom'
 
-
     def __init__(self, name):
         self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name, MoveGripperAction, execute_cb=self.execute_cb, auto_start=False)
-        self._as.start()
+        self._as = actionlib.SimpleActionServer(self._action_name, MoveGripperAction, execute_cb=self.execute_cb,
+                                                auto_start=False)
         self._giskard_wrapper = GiskardWrapper()
-        print("MoveGripperActionServer greats its masters and is waiting for orders")
+        self._as.start()
 
     def execute_cb(self, goal):
-        ## Integrate giskard here
-        print("Order recieved: move")
+        rospy.loginfo("Move gripper: {}".format(goal))
         self._result.error_code = self._result.FAILED
-
         self._giskard_wrapper.interrupt()
-
         self._giskard_wrapper.set_cart_goal(self._root, u'hand_palm_link', goal.goal_pose)
         self._giskard_wrapper.plan_and_execute(wait=False)
-
-        #TODO: send feedback periodically?
-
         result = self._giskard_wrapper.get_result(rospy.Duration(60))
-
-        self._feedback.tf_gripper_to_goal = tfwrapper.lookup_transform(goal.goal_pose.header.frame_id, u'hand_palm_link')
-
-        self._as.publish_feedback(self._feedback)
-
-        if result and result.error_code == result.SUCCESS:
+        if result and result.SUCCESS in result.error_code:
             self._result.error_code = self._result.SUCCESS
-
         self._as.set_succeeded(self._result)
 
 
