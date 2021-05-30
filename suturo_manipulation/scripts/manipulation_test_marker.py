49ada0e00c90dd7b37a3f1821cba690c67084ded
clement <clement1@uni-bremen.de>
2021-05-30 22:23:52 +0200

    added missing documentation, moved parameters to the hsr_param.yaml and increased the weight of the arms_roo_joint to improve the stability, when opening doors

--- suturo_manipulation/scripts/manipulation_test_marker.py
+++ suturo_manipulation/scripts/manipulation_test_marker.py
@@ -146,7 +146,7 @@ def place_top_cb(feedback):
     
 
 def open_shelve_1_cb(feedback):
-    open(u'iai_kitchen/kitchen:outside:door_handle_outside', u'iai_kitchen/kitchen:outside:door_handle_outside')
+    open(u'iai_kitchen/hsr_shelf_openable:shelf:shelf_door_left:shelf_link_handle', u'iai_kitchen/hsr_shelf_openable:shelf:shelf_door_left:shelf_link_handle')
 
 
 def open_door_1_cb(feedback):
