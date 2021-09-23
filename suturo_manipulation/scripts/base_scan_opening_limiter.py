b25f124e97669f28d9997e03a671c54ff8156fb0
clement <clement1@uni-bremen.de>
2021-09-23 21:11:55 +0200

    still missing documentation

--- suturo_manipulation/scripts/base_scan_opening_limiter.py
+++ suturo_manipulation/scripts/base_scan_opening_limiter.py
@@ -7,17 +7,30 @@ from std_srvs.srv import SetBool, SetBoolResponse
 from sensor_msgs.msg import LaserScan
 
 class BaseScanLimiterServer:
+    """
+    Action Server, which limits the laser scanner, when the HSR opens a door.
+    """
     IGNORE_AREA = 55
     limit_area = False
     laser_scan_publisher = rospy.Publisher('hsrb/base_scan', LaserScan)
     limit_right_side = True
 
     def __init__(self, name):
+        """
+        Inits the action server.
+        :param name The name of the action server
+        :type name string
+        """
         self._action_name = name
         laser_scan_subscriber = rospy.Subscriber('/hsrb/base_scan_unlimited', LaserScan, self.laser_scan_callback)
         service_server = rospy.Service('/base_scan_limitation', SetBool, self.enable_limitation)
 
     def laser_scan_callback(self, msg):
+        """
+        Receives the laser data and limits it, when needed.
+        :param msg The laser scan message
+        :type msg LaserScan
+        """
         if self.limit_area:
             sensor_ranges = np.array(msg.ranges)
             sensor_middle = len(sensor_ranges) / 2
@@ -35,6 +48,11 @@ class BaseScanLimiterServer:
         self.laser_scan_publisher.publish(msg)
 
     def enable_limitation(self, request):
+        """
+        Enables/Disables the laser scan limitation mechanism.
+        :param request data, containing which side to limit
+        :type request SetBool
+        """
         self.limit_area = not self.limit_area
         self.limit_right_side = request.data
         return SetBoolResponse(
