013ae3769dfaf900e24670901b0ad2d9a78134c1
K3cks <philk99@web.de>
2023-06-14 16:13:41 +0200

    updated test script

--- suturo_manipulation/scripts/milestone1_showcase.py
+++ suturo_manipulation/scripts/milestone1_showcase.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
-import time
-from typing import List
 
-import hsrb_interface
 import rospy
-import math  # for sin and cos
-from geometry_msgs.msg import PoseStamped, Point, Quaternion, Vector3Stamped, PointStamped, Vector3
+from geometry_msgs.msg import PoseStamped, Quaternion, PointStamped, Vector3
 
 from giskardpy.python_interface import GiskardWrapper
 
@@ -49,22 +45,26 @@ def get_entity(ent_name: str,
             return ent
 
 
-
 def prepare_variables():
-
     # Objects
     # mueslibox position
     mueslibox_center = PoseStamped()
     mueslibox_center.header.frame_id = 'map'
     mueslibox_center.pose.position.x = - 0.1
     mueslibox_center.pose.position.y = 1.68
-    mueslibox_center.pose.position.z = 0.7
+    mueslibox_center.pose.position.z = 0.9
 
     # medium muesli size
     medium_muesli_size = Vector3()
     medium_muesli_size.x = 0.14
     medium_muesli_size.y = 0.062
     medium_muesli_size.z = 0.22
+
+    # Spoon size
+    spoon_size = Vector3()
+    spoon_size.x = 0.06
+    spoon_size.y = 0.02
+    spoon_size.z = 0.04
 
     # big muesli size
     big_muesli_size = Vector3()
@@ -88,11 +88,13 @@ def prepare_variables():
     medium_muesli = Box(name='muesli_medium', size=medium_muesli_size)
     big_muesli = Box(name='muesli_big', size=big_muesli_size)
     drawer_knob = Box(name='drawer_knob', size=knob_size)
+    spoon = Box(name='spoon', size=spoon_size)
 
     objects = []
     objects.append(medium_muesli)
     objects.append(big_muesli)
     objects.append(drawer_knob)
+    objects.append(spoon)
 
     # Single Positions
     # Simulation test pose
@@ -147,16 +149,14 @@ def pick_object(name: str,
     '''
     # Pick object
     print('Getting in position')
-    offset = 0.01
     _giskard_wrapper.grasp_object(object_name=name,
                                   object_pose=pose,
                                   object_size=size,
                                   root_link=root_link,
-                                  tip_link=tip_link,
-                                  offset=offset)
+                                  tip_link=tip_link)
 
-    #_giskard_wrapper.plan_and_execute(wait=True)
-    #_giskard_wrapper.plan()
+    _giskard_wrapper.plan_and_execute(wait=True)
+    # _giskard_wrapper.plan()
     '''
     # Attach Object
     #_giskard_wrapper.update_parent_link_of_group(name, tip_link)
@@ -167,14 +167,18 @@ def pick_object(name: str,
     
     # Lift Object
     print('Lifting Object')
+    new_start = PointStamped()
+    new_start.point.z = 0.3
+    
     _giskard_wrapper.lift_object(object_name=name)
     _giskard_wrapper.plan_and_execute(wait=True)
     
     # Retract
     print('Retracting')
-    _giskard_wrapper.retract(object_name=object_name)
+    _giskard_wrapper.retract(object_name=object_name, distance=0.1)
     _giskard_wrapper.plan_and_execute(wait=True)
-
+    
+    
     set_base_position()
     _giskard_wrapper.plan_and_execute(wait=True)
     '''
@@ -189,34 +193,27 @@ def open_gripper():
 def place_object(name: str,
                  pose: PoseStamped,
                  height: float,
+                 radius=0.0,
                  root_link='map',
-                 tip_link='hand_palm_link'):
+                 tip_link='hand_palm_link',
+                 height_only=True,
+                 frontal=True):
     # Align height
-    _giskard_wrapper.prepare_placing(object_pose=pose)
-    _giskard_wrapper.plan_and_execute(wait=True)
+    '''_giskard_wrapper.align_height(object_name=name,
+                                  object_pose=pose,
+                                  height=height,
+                                  tip_link=tip_link,
+                                  height_only=height_only)
+    _giskard_wrapper.plan_and_execute(wait=True)'''
 
     # Place Object
     _giskard_wrapper.place_object(object_name=name,
                                   goal_pose=pose,
                                   object_height=height,
+                                  radius=radius,
                                   root_link=root_link,
-                                  tip_link=tip_link)
-    _giskard_wrapper.plan_and_execute(wait=True)
-
-    # Open Gripper
-
-    open_gripper()
-
-    # Detach Object
-    print('Move Back')
-    _giskard_wrapper.update_parent_link_of_group(name, root_link)
-    _giskard_wrapper.avoid_collision(min_distance=0.01, group1=_giskard_wrapper.robot_name, group2=name)
-    _giskard_wrapper.retract(object_name=name)
-
-    _giskard_wrapper.plan_and_execute(wait=True)
-
-    set_base_position()
-
+                                  tip_link=tip_link,
+                                  frontal=frontal)
     _giskard_wrapper.plan_and_execute(wait=True)
 
 
@@ -264,25 +261,20 @@ def add_object(name: str,
 def test_new_feature(name: str,
                      pose: PoseStamped,
                      size, grasp, lift_first):
+    # add_object(name=name, pose=pose, size=size)
 
-
-    #add_object(name=name, pose=pose, size=size)
-
-    rotation = 'TestRotationGoal'
     sequence = 'SequenceGoal'
     force_sensor = 'TestForceSensorGoal'
-    tilt = 'Tilting'
 
     test_goal = sequence
 
-    #pose.pose.position.x = 1.48
-    #pose.pose.position.y = -0.979
-    #pose.pose.position.z = 0.7
-
+    # pose.pose.position.x = 1.48
+    # pose.pose.position.y = -0.979
+    # pose.pose.position.z = 0.7
 
     pose_1 = PoseStamped()
-    pose_1.pose.position.x = 0.0
-    pose_1.pose.position.y = 0.5
+    pose_1.pose.position.x = 0.5
+    pose_1.pose.position.y = 0.0
     pose_1.pose.position.z = 0.7
 
     pose_2 = PoseStamped()
@@ -290,28 +282,68 @@ def test_new_feature(name: str,
     pose_2.pose.position.y = 1.0
     pose_2.pose.position.z = 0.7
 
+    place_object_args = {'object_name': '',
+                         'target_pose': pose_1,
+                         'object_height': 0.0}
 
-    _giskard_wrapper.test_goal(test_goal, object_name=name, object_pose_1=pose_1, object_pose_2=pose_2, grasp_object=grasp, lift_first=lift_first)
-    #_giskard_wrapper.retract(object_name='')
+    lifting1_point = PointStamped()
+    lifting1_point.header.frame_id = 'hand_palm_link'
+    lifting1_point.point.x += 0.05
 
-    #_giskard_wrapper.add_cmd()
+    retracting1_point = PointStamped()
+    retracting1_point.header.frame_id = 'hand_palm_link'
+    retracting1_point.point.z -= 0.1
 
-    #_giskard_wrapper.lift_object(object_name='')
+    object_name_args = {'object_name': '', 'lifting': 0.05}
 
-    #_giskard_wrapper.move_gripper(open_gripper=False)
+    retract_args = {'object_name': '',
+                    'distance': 0.1,
+                    'tip_link': 'base_link'}
 
-    #joints = {'arm_lift_joint': 0.0}
+    object_name_args_2 = {'object_name': 'second',
+                          'lifting': 0.06}
 
-    #_giskard_wrapper.set_joint_goal(goal_state=joints)
+    align_height_args = {'object_name': name,
+                         'goal_pose': pose_1,
+                         'object_height': 0.0,
+                         'height_only': True}
 
-    #_giskard_wrapper.prepare_placing(object_pose=test_position.pose, height=size.z)
+    grasp_object_args = {'object_name': name,
+                         'object_pose': pose,
+                         'object_size': size}
+
+    goal_type_seq = ['AlignHeight', 'GraspObject']
+    kwargs_seq = [align_height_args, grasp_object_args]
+
+    _giskard_wrapper.test_goal(test_goal,
+                               object_name=name,
+                               object_pose_1=pose_1,
+                               object_pose_2=pose_2,
+                               grasp_object=grasp,
+                               lift_first=lift_first,
+                               goal_type_seq=goal_type_seq,
+                               kwargs_seq=kwargs_seq)
+
+    # _giskard_wrapper.retract(object_name='')
+
+    # _giskard_wrapper.add_cmd()
+
+    # _giskard_wrapper.lift_object(object_name='')
+
+    # _giskard_wrapper.move_gripper(open_gripper=False)
+
+    # joints = {'arm_lift_joint': 0.0}
+
+    # _giskard_wrapper.set_joint_goal(goal_state=joints)
+
+    # _giskard_wrapper.align_height(object_pose=test_position.pose, height=size.z)
 
     # Move gripper, theoretically
     # joints = {'hand_motor_joint': 1.0}
     # _giskard_wrapper.set_joint_goal(goal_state=joints)
 
     _giskard_wrapper.plan_and_execute()
-    #_giskard_wrapper.plan()
+    # _giskard_wrapper.plan()
 
 
 if __name__ == '__main__':
@@ -319,8 +351,8 @@ if __name__ == '__main__':
     _giskard_wrapper = GiskardWrapper()
     objects, positions = prepare_variables()
 
-    object_name = 'muesli_medium'
-    position_name = 'LabEnv_table'
+    object_name = 'spoon'
+    position_name = 'simulation_test_pose'
     tf_name = 'Shelf_OGTVKLRY'
     test_object = get_entity(object_name, objects)
     test_position: Position = get_entity(position_name, positions)
@@ -328,29 +360,35 @@ if __name__ == '__main__':
     print(test_object.size)
     print(test_position.pose)
 
-    pick_object(name=tf_name, pose=test_position.pose, size=test_object.size)
+    #pick_object(name=tf_name, pose=test_position.pose, size=test_object.size)
 
     # create object
-    #add_object(name=tf_name, pose=test_position.pose, size=test_object.size)
+    # add_object(name=tf_name, pose=test_position.pose, size=test_object.size)
 
     # place object
-    #place_object(name=object_name, pose=test_position.pose, height=test_object.height)
+    rad = 0.0
+    #place_object(name=object_name, pose=test_position.pose, height=test_object.height, radius=rad, height_only=True)
+
+    #set_base_position()
 
     # Drawer
     #pick_object(name='', pose=test_position.pose, size=test_object.size)
 
     # Gripper
-    #test_new_feature(tf_name, test_position.pose, test_object.size, grasp=True, lift_first=False)
+    test_new_feature(tf_name, test_position.pose, test_object.size, grasp=True, lift_first=False)
 
-    #_giskard_wrapper.lift_object(object_name='')
+    # giskard_wrapper.lift_object(object_name='')
 
-    #goal_p = PoseStamped()
-    #goal_p.pose.position.x = 0.08
-    #goal_p.pose.position.y = 1.75
-    #goal_p.pose.position.z = 0.725
+    # goal_p = PoseStamped()
+    # goal_p.pose.position.x = 0.08
+    # goal_p.pose.position.y = 1.75
+    # goal_p.pose.position.z = 0.725
 
-    #height = 0.22
-    #_giskard_wrapper.place_object(object_name='', goal_pose=goal_p, object_height=height)
-    #_giskard_wrapper.tilt()
-    #_giskard_wrapper.move_gripper(open_gripper=False)
-    _giskard_wrapper.plan_and_execute()
+    # height = 0.22
+    # _giskard_wrapper.place_object(object_name='', goal_pose=goal_p, object_height=height)
+    # _giskard_wrapper.tilt()
+    # _giskard_wrapper.move_gripper(open_gripper=False)
+
+    # _giskard_wrapper.align_height(object_name='', object_pose=test_position.pose, height=test_object.height)
+
+    # _giskard_wrapper.plan_and_execute()
